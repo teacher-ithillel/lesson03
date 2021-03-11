e1ab1a220ebcfe31e8d6ca5a5c6c4ae8ae856be3
Evhen <oeasoft@gmail.com>
2021-03-11 16:15:00 +0200

    [HW-03] Implementation fixes.

--- src/ua/ithillel/ArrayUtils.java
+++ src/ua/ithillel/ArrayUtils.java
@@ -12,40 +12,50 @@ public class ArrayUtils {
 
         //System.out.println(Arrays.toString(objArr));
 
-        StringBuilder out = new StringBuilder();
-        for (int c = 0; c < objArr.length; c++) {
-            if (c == ((objArr.length) - 1)) out.append(objArr[c]);
-            else out.append(objArr[c]).append(", ");
+        StringBuilder out = new StringBuilder("["); //
+        for (int i = 0; i < objArr.length; i++) {
+            if (i == ((objArr.length) - 1)) {
+                out.append(objArr[i]).append("]");
+            } else {
+                out.append(objArr[i]).append(", ");
+            }
         }
-        System.out.println("[" + out + "]");
+        System.out.println(out);
     }
 
     public static void printIntArray(int[] intArr) {
-        if (testArr(intArr)) return;
+        if (testArr(intArr)) {
+            return;
+        }
 
         //System.out.println(Arrays.toString(intArr));
 
-        StringBuilder out = new StringBuilder();
-        for (int c = 0; c < intArr.length; c++) {
-            if (c == ((intArr.length) - 1)) out.append(intArr[c]);
-            else out.append(intArr[c]).append(", ");
+        StringBuilder out = new StringBuilder("[");
+        for (int i = 0; i < intArr.length; i++) {
+            if (i == ((intArr.length) - 1)) {
+                out.append(intArr[i]).append("]");
+            } else {
+                out.append(intArr[i]).append(", ");
+            }
         }
-        System.out.println("[" + out + "]");
+        System.out.println(out);
     }
 
     public static int[] sortIntArray(int[] intArr) {
-        if (testArr(intArr)) return new int[0];
+        if (testArr(intArr)) {
+            return new int[0];
+        }
 
         /*Arrays.sort(intArr);
         return intArr;*/
 
         //Sorting method: Bubble Sort.
         for (int i = intArr.length - 1; i > 0; i--) {
-            for (int c = 0; c < i; c++) {
-                if (intArr[c] > intArr[c + 1]) {
-                    int temp = intArr[c];
-                    intArr[c] = intArr[c + 1];
-                    intArr[c + 1] = temp;
+            for (int j = 0; j < i; j++) {
+                if (intArr[j] > intArr[j + 1]) {
+                    int temp = intArr[j];
+                    intArr[j] = intArr[j + 1];
+                    intArr[j + 1] = temp;
                 }
             }
         }
@@ -53,7 +63,9 @@ public class ArrayUtils {
     }
 
     public static int[] findMinAndMaxFromIntArray(int[] intArr) {
-        if (testArr(intArr)) return new int[0];
+        if (testArr(intArr)) {
+            return new int[0];
+        }
 
         /*int[] minMax = new int[2];
         minMax[0] = Arrays.stream(intArr).min().getAsInt();
@@ -62,24 +74,29 @@ public class ArrayUtils {
 
         int[] minMax = new int[2];
         for (int i : intArr) {
-            if (i < minMax[0]) minMax[0] = i;
-            if (i > minMax[1]) minMax[1] = i;
+            if (i < minMax[0]) {
+                minMax[0] = i;
+            } else if (i > minMax[1]) {
+                minMax[1] = i;
+            }
         }
         return minMax;
     }
 
     public static String toOneStringInCamelCase(String[] strArr) {
-        if (!testArr(strArr)) return null;
-        String outStr = "";
-        if (strArr[0] != null) {
-            outStr = strArr[0].substring(0, 1).toLowerCase() + strArr[0].substring(1);
+        if (!testArr(strArr)) {
+            return null;
         }
-        for (int c = 1; c < strArr.length; c++) {
-            if (strArr[c] != null) {
-                outStr = outStr.concat(strArr[c].substring(0, 1).toUpperCase()).concat(strArr[c].substring(1));
+        StringBuilder outStr = new StringBuilder();
+        if (strArr[0] != null) {
+            outStr.append(strArr[0].substring(0, 1).toLowerCase()).append(strArr[0].substring(1));
+        }
+        for (int i = 1; i < strArr.length; i++) {
+            if (strArr[i] != null) {
+                outStr.append(strArr[i].substring(0, 1).toUpperCase()).append(strArr[i].substring(1));
             }
         }
-        return outStr;
+        return new String(outStr);
     }
 
     /**
@@ -89,12 +106,16 @@ public class ArrayUtils {
      * @return the int
      */
     public static int calculateSumElementsInIntArray(int[] intArr) {
-        if (testArr(intArr)) return 0;
+        if (testArr(intArr)) {
+            return 0;
+        }
 
         //return Arrays.stream(intArr).sum();
 
         int sum = 0;
-        for (int i : intArr) sum += i;
+        for (int i : intArr) {
+            sum += i;
+        }
         return sum;
     }
 
@@ -105,38 +126,54 @@ public class ArrayUtils {
         return ArrayUtils.sortIntArray(out);*/
 
         int[] result = new int[ints1.length + ints2.length];
-        for (int c = 0; c < ints1.length; c++) result[c] = ints1[c];
-        for (int c = 0; c < ints2.length; c++) result[ints1.length + c] = ints2[c];
+        for (int i = 0; i < ints1.length; i++) {
+            result[i] = ints1[i];
+        }
+        for (int i = 0; i < ints2.length; i++) {
+            result[ints1.length + i] = ints2[i];
+        }
         return ArrayUtils.sortIntArray(result);
     }
 
     public static int calculateAbsAverageElementValueInIntArray(int[] intArr) {
-        if (testArr(intArr)) return 0;
+        if (testArr(intArr)) {
+            return 0;
+        }
 
         /*int sum = 0;
-        for (int i : intArr) sum += Math.abs(i);
+        for (int i : intArr) {
+            sum += Math.abs(i);
+        }
+        sum = (sum / intArr.length);
         return sum;*/
 
         int sum = 0;
         for (int i : intArr) {
-            if (i < 0) i = i * (-1);
+            if (i < 0) {
+                i = i * (-1);
+            }
             sum += i;
         }
+        sum = (sum / intArr.length);
         return sum;
     }
 
     public static int[] removeElementFromIntArray(int i, int[] ints) {
-        if (testArr(ints)) return new int[0];
+        if (testArr(ints)) {
+            return new int[0];
+        }
 
         int newLength = ints.length;
-        for (int c = 0; c < ints.length; c++) {
-            if (ints[c] == i) newLength--;
+        for (int j = 0; j < ints.length; j++) {
+            if (ints[j] == i) {
+                newLength--;
+            }
         }
         int[] result = new int[newLength];
         int index = 0;
-        for (int c = 0; c < ints.length; c++) {
-            if (ints[c] != i) {
-                result[index] = ints[c];
+        for (int j = 0; j < ints.length; j++) {
+            if (ints[j] != i) {
+                result[index] = ints[j];
                 index++;
             }
         }
@@ -149,7 +186,9 @@ public class ArrayUtils {
             return new int[0];
         }
         int[] out = new int[size];
-        for (int i = 0; i < size; i++) out[i] = (int) (Math.random() * (max - min + 1) + min);
+        for (int i = 0; i < size; i++) {
+            out[i] = (int) (Math.random() * (max - min + 1) + min);
+        }
         return out;
     }
 
