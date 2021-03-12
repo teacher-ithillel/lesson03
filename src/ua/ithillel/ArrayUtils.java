246875b3d4ea0903d3fa4c3fcdab7eca275924f8
Ihor Zhurbuk <zhurbuk1718@gmail.com>
2021-03-12 14:56:56 +0300

    Realizeted Arrays

--- src/ua/ithillel/ArrayUtils.java
+++ src/ua/ithillel/ArrayUtils.java
@@ -11,50 +11,123 @@ public class ArrayUtils {
     массив будет отсортирован по возрастанию.
      */
     public static void printObjArray(Object[] objArr) {
-        // TODO: implement this method
+        System.out.println(Arrays.toString(objArr));
     }
 
     public static void printIntArray(int[] intArr) {
-        // TODO: implement this method
+
+        System.out.println(Arrays.toString(intArr));
     }
 
     public static int[] sortIntArray(int[] intArr) {
-        return new int[]{1, 2, 3, 4, 5};
-        // TODO: implement this method
+        int povtoriv = 0;//кількість повторів
+        boolean zamina = true;//
+        while (zamina) {
+            zamina = false;// означае що масив відсортовано
+            for (int i = 0; i < intArr.length - 1 - povtoriv; i++) {// віднімаю к-сть повторів щоб цикл не проходив до кінця так як найбільший елемент гарантовано після першого пробігу стане останім
+                if (intArr[i] > intArr[i + 1]) {// порівнюю сусідні елемнти
+                    int tmp = intArr[i];// tmp тимчасова змінна для перестановки елеметів
+                    intArr[i] = intArr[i + 1];
+                    intArr[i + 1] = tmp;
+                    zamina = true;// кажу що заміна відбулась
+                }
+            }
+            povtoriv++;
+        }
+
+        return intArr;
     }
 
     public static int[] findMinAndMaxFromIntArray(int[] intArr) {
-        return new int[]{-1, 1};
-        // TODO: implement this method
+        Arrays.sort(intArr);
+        return new int[]{intArr[0], intArr[intArr.length - 1]};
+
     }
 
     public static String toOneStringInCamelCase(String[] strArr) {
+
         return null;
-        // TODO: implement this method
+
     }
 
     public static int calculateSumElementsInIntArray(int[] intArr) {
-        return 0;
-        // TODO: implement this method
+        int sum = 0;
+        for (int i = 0; i < intArr.length; i++) {
+            sum += intArr[i];
+        }
+        return sum;
     }
 
     public static int[] mergeAndSortTwoIntArray(int[] ints1, int[] ints2) {
-        return new int[0];
-        // TODO: implement this method
+        int ints1Length = ints1.length;
+        int inst2Length = ints2.length;
+
+        int[] merged = new int[ints1Length + inst2Length];
+
+        int ints1Position, ints2Position, mergedPosition;
+        ints1Position = ints2Position = mergedPosition = 0;
+
+        while (ints1Position < ints1Length && ints2Position < inst2Length) {// прохожусь по обом масивам і беру масив який має елемент з меншим значееням свого індексу
+            if (ints1[ints1Position] < ints2[ints2Position]) {
+                merged[mergedPosition++] = ints1[ints1Position++];
+            } else {
+                merged[mergedPosition++] = ints2[ints2Position++];
+            }
+        }
+
+        while (ints1Position < ints1Length) {// копіюю залишені елементи
+            merged[mergedPosition++] = ints1[ints1Position++];
+        }
+
+        while (ints2Position < inst2Length) {// копіюю залишені елементи
+            merged[mergedPosition++] = ints2[ints2Position++];
+        }
+
+        return merged;
     }
 
+
     public static int calculateAbsAverageElementValueInIntArray(int[] intArr) {
-        return 0;
-        // TODO: implement this method
+        if (intArr == null)
+            return 0;
+
+        int sum = 0;
+        int n = 0;
+
+        for (int i = 0; i < intArr.length; i++)
+            if (intArr[i] > 0) {
+                sum += intArr[i];
+                n++;
+            }
+
+        return (sum / n);
+
     }
 
     public static int[] removeElementFromIntArray(int i, int[] ints) {
-        return new int[0];
-        // TODO: implement this method
+        int[] Array = new int[ints.length - 1];
+
+        for (int k = 0, j = 0; k < ints.length; k++) {
+            if (ints[k] == i) {
+                ints[k] = 0;
+
+            } else {
+                Array[j++] = ints[k];
+            }
+        }
+        return Array;
     }
 
+
     public static int[] generateRandomIntArrayWithSizeMinMax(int size, int min, int max) {
-        return new int[0];
-        // TODO: implement this method
+        int[] result = new int[size];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = (int) ((Math.random() * (max - min)) + min);
+        }
+
+        return result;
     }
+
+
 }
